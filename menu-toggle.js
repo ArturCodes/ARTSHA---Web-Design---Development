84b88a9f4f4d5bd0816ac75679b32342b4041a49
Artur Shapiro <arturshapiro@gmail.com>
2023-06-09 00:48:56 -0500

    .

--- menu-toggle.js
+++ menu-toggle.js
@@ -3,11 +3,7 @@ const menuButton = document.getElementById('.menu-toggle');
 
 function toggleMenu() {
 
-    console.log('test');
-
     lines.forEach(line => {
         line.classList.toggle('active');
     });
 }
-
-menuButton.addEventListener('click', toggleMenu);
