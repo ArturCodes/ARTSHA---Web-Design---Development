75bf4bc1c3f8c6bbb2b74d480656c4debc2b8121
Artur Shapiro <arturshapiro@gmail.com>
2023-06-09 06:32:19 -0500

    .

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,9 +1,15 @@
 const lines = document.querySelectorAll('.line');
 const menuButton = document.getElementById('.menu-toggle');
+const opacityBG = document.getElementById('opacity-bg');
+const mobileNav = document.getElementById('navbar-links');
 
 function toggleMenu() {
+    // opacityBG.classList.toggle('show');
 
     lines.forEach(line => {
         line.classList.toggle('active');
     });
+
+    mobileNav.classList.toggle('mobile-nav-active');
+
 }
