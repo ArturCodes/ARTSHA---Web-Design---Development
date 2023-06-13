fa8dc75b148ea676bd8609478053d5ea659b2a09
ArturCodes <arturshapiro@gmail.com>
2023-06-13 11:47:58 -0500

    .

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,9 +1,9 @@
 const lines = document.querySelectorAll('.line');
 const opacityBG = document.getElementById('opacity-bg');
 const mobileNav = document.getElementById('navbar-links');
-const menuButton = document.getElementById('menu-toggle-button');
 
 function toggleMenu() {
+
     lines.forEach(line => {
         line.classList.toggle('active');
     });
